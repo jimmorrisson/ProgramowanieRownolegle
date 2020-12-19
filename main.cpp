9e69cc6171eb950410d88f3f5b4bbb22f8224502
jimmorrisson <pyszczakjakub@gmail.com>
2020-12-19 21:56:52 +0100

    Add openmp library

--- main.cpp
+++ main.cpp
@@ -5,22 +5,35 @@
 #include "QuadraticFunction.h"
 #include "PowerSingularFunction.h"
 #include "TestUtil.h"
+#include <memory>
+#include <omp.h>
+#include <iostream>
 
 int main()
 {
     TestUtil testUtilLib(1e-6, 1000, true);
     TestUtil testUtilAlg(1e-6, 1000, false);
 
-    testUtilLib.runTest(LBFGSExampleRosenbrock(60));
-    testUtilAlg.runTest(LBFGSExampleRosenbrock(60));
-    
-    testUtilLib.runTest(ExtendedRosenbrockFunction(60));
-    testUtilAlg.runTest(ExtendedRosenbrockFunction(60));
+    std::unique_ptr<IFunction> exampleRosenbrock = std::make_unique<LBFGSExampleRosenbrock>(60);
+    testUtilLib.runTest(*exampleRosenbrock.get());
+    testUtilAlg.runTest(*exampleRosenbrock.get());
 
-    testUtilLib.runTest(QuadraticFunction(60));
-    testUtilAlg.runTest(QuadraticFunction(60));
+    std::unique_ptr<IFunction> extendedRosenbrock = std::make_unique<ExtendedRosenbrockFunction>(60);
+    testUtilLib.runTest(*extendedRosenbrock.get());
+    testUtilAlg.runTest(*extendedRosenbrock.get());
 
-    testUtilLib.runTest(PowerSingularFunction(60));
-    testUtilAlg.runTest(PowerSingularFunction(60));
+    std::unique_ptr<IFunction> quadraticFunction = std::make_unique<QuadraticFunction>(60);
+    testUtilLib.runTest(*quadraticFunction.get());
+    testUtilAlg.runTest(*quadraticFunction.get());
+
+    std::unique_ptr<IFunction> powerSingularFunction = std::make_unique<PowerSingularFunction>(60);
+    testUtilLib.runTest(*powerSingularFunction.get());
+    testUtilAlg.runTest(*powerSingularFunction.get());
+
+    #pragma omp parallel num_threads(4)
+    {
+        #pragma omp critical
+        std::cout << "tid = " << omp_get_thread_num() << std::endl;
+    }
     return 0;
 }
